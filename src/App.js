d0282a3ebc01fad78bc9abc0bebc9f754693fd57
Pavel Malkov <pavelmalkov0z@gmail.com>
2021-10-21 21:41:43 +0300

    21

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react"
 import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom"
 import { SvgSelector } from "./components/svg/svgSelector.tsx";
 import logo from './svg/Company\'s logo.svg';
+import person from './img/photo.png';
 import activesection from './svg/activesection.svg';
+
+import { SvgIcon } from "./components/svg/SvgIcon";
 
 import "./css/panel.css"
 import "./css/grid.css"
 import "./css/defoult.css"
 import "./css/area.css"
 import "./css/navlink.css"
+import "./css/navhelp.css"
 
 import MyInput from "./components/UI/myinput"
 
@@ -26,34 +30,55 @@ function App() {
     <div className="App">
       <Router>
         <div className="container">
-          <div className="nav link whiteArea">
+          <div className="left whiteArea">
             <div className="logo">
               <img
               src={logo}
               width="50"
               height="50"/>
             </div>
-            <ul>
-              <NavLink to = "/Dashboard"   activeClassName="onClicklink"> <li><SvgSelector id = "dashboard"/><a>Dashboard  </a><img src={activesection}/></li></NavLink>
-              <NavLink to = "/Projects"    activeClassName="onClicklink"> <li><SvgSelector id = "dashboard"/><a>Projects   </a><img src={activesection}/></li></NavLink>
-              <NavLink to = "/Calendar"    activeClassName="onClicklink"> <li><SvgSelector id = "dashboard"/><a>Calendar   </a><img src={activesection}/></li></NavLink>
-              <NavLink to = "/Vacations"   activeClassName="onClicklink"> <li><SvgSelector id = "dashboard"/><a>Vacations  </a><img src={activesection}/></li></NavLink>
-              <NavLink to = "/Employees"   activeClassName="onClicklink"> <li><SvgSelector id = "dashboard"/><a>Employees  </a><img src={activesection}/></li></NavLink>
-              <NavLink to = "/Messenger"   activeClassName="onClicklink"> <li><SvgSelector id = "dashboard"/><a>Messenger  </a><img src={activesection}/></li></NavLink>
-              <NavLink to = "/Info Portal" activeClassName="onClicklink"> <li><SvgSelector id = "dashboard"/><a>Info Portal</a><img src={activesection}/></li></NavLink>
-            </ul>
+            <div className="link">
+              <ul>
+                <NavLink to = "/Dashboard"   activeClassName="onClicklink"> <li><SvgIcon id = "dashboard"/><a>Dashboard  </a><img src={activesection}/></li></NavLink>
+                <NavLink to = "/Projects"    activeClassName="onClicklink"> <li><SvgIcon id = "dashboard"/><a>Projects   </a><img src={activesection}/></li></NavLink>
+                <NavLink to = "/Calendar"    activeClassName="onClicklink"> <li><SvgIcon id = "dashboard"/><a>Calendar   </a><img src={activesection}/></li></NavLink>
+                <NavLink to = "/Vacations"   activeClassName="onClicklink"> <li><SvgIcon id = "dashboard"/><a>Vacations  </a><img src={activesection}/></li></NavLink>
+                <NavLink to = "/Employees"   activeClassName="onClicklink"> <li><SvgIcon id = "dashboard"/><a>Employees  </a><img src={activesection}/></li></NavLink>
+                <NavLink to = "/Messenger"   activeClassName="onClicklink"> <li><SvgIcon id = "dashboard"/><a>Messenger  </a><img src={activesection}/></li></NavLink>
+                <NavLink to = "/Info Portal" activeClassName="onClicklink"> <li><SvgIcon id = "dashboard"/><a>Info Portal</a><img src={activesection}/></li></NavLink>
+              </ul>
+            </div>
+
+            <div className = "navhelp">
+              <div> <button><SvgIcon id = "message"/>Support</button> </div>
+              <div className = "illustration"><SvgIcon id = "illustration" /></div>
+            </div>
+
+            <div className="link">
+              <ul>
+                <NavLink to = "/Exit"   activeClassName="onClicklink"> <li><SvgIcon id = "dashboard"/><a>Exit</a><img src={activesection}/></li></NavLink>
+              </ul>
+            </div>
+
           </div>
           <div className = "panel">
             <div className = "child-begin"><MyInput /></div>
-            <div className = "child-space"><SvgSelector id = "notifications"/></div>
-            <div className = "child-end"><h1>Мой профиль</h1></div>
+            <div className = "child-space whiteArea notification"><SvgIcon id = "notifications"/></div>
+            <div className="child-end whiteArea account">
+                <div className = ""><img src = {person} alt = "photo" /></div>
+                <div className = ""><p>Иван Иванович</p></div>
+                <div className = ""><span className = "chevron-down"><SvgIcon id = "right"/></span></div>
+            </div>
           </div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/Dashboard" component={Dashboard} />
-            <Route path="/Projects" component={Projects} />
-            <Route component={NotFound} />
-          </Switch>
+          
+          <div className="whiteArea">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/Dashboard" component={Dashboard} />
+              <Route path="/Projects" component={Projects} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
         </div>  
       </Router>
     </div>
@@ -61,48 +86,3 @@ function App() {
 }
 
 export default App;
-
-/*
-<div className="header">
-  <div className = "one">
-    <h1>шапка поиск</h1>
-    <h1>мой профиль и настройки</h1>
-  </div>
-  <div className = "two">
-    <div className="container">
-      <div className="nav whiteArea">
-        <h1>список ссылок</h1>
-      </div>
-      <div className="header whiteArea"></div>
-    </div>
-  </div>
-</div>
-*/
-
-{/* 
-
-  <div className="two">
-        <div class="left whiteArea">Навигационная панель</div>
-        <div class="right">
-          <h1>Поиск и мой профиль</h1>
-          <h1>Мой профиль и настройки</h1>
-
-          <div className="two">
-            <div class="left whiteArea">Описание пользователя</div>
-            <div class="right">
-              <h1>настройки</h1>
-
-              <div className="two">
-                <div class="left whiteArea">список изменяемых данных</div>
-                <div class="right whiteArea">
-                  <h1>изменения</h1>
-                </div>
-              </div>
-
-            </div>
-          </div>
-
-        </div>
-      </div>
-
-*/}
--- src/components/UI/myinput.js
+++ src/components/UI/myinput.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import "./myinput.css"
 import { SvgSelector } from '../svg/svgSelector'
+import { SvgIcon } from '../svg/SvgIcon'
+
 
 export default class MyInput extends Component {
     render() {
         return (
             <div className="content">
-                <SvgSelector id = "search"/>
+                <SvgIcon id = "search"/>
                 <input className = "input" type="text" size="40" placeholder = "Поиск" />
             </div>
         )
